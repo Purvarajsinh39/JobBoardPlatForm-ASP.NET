7f450b3b982f5eccb2e0761a1966e44d810f4bdc
Purvarajsinh39 <vaghelapurvarajsinh8442@gmail.com>
2025-07-30 10:54:23 +0530

    27-7-2025

---
+++ JobBoardPlatform/Models/JobWithEmployerViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JobBoardPlatform.Models
+{
+    public class JobWithEmployerViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public string Location { get; set; }
+        public DateTime PostedDate { get; set; }
+        public bool IsApproved { get; set; }
+        public string EmployerName { get; set; }
+    }
+}
